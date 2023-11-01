5b8a2bfd94d2772e1f63a7362909aaf92e331c59
kartiksongara <kartiksongara2000@gmail.com>
2023-11-01 20:01:10 +0530

    jsfile

---
+++ starpattern.js
@@ -0,0 +1,182 @@
+/*using nested for loop 
+*/
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(String("*"))
+    }
+    console.log("")
+}                                                                                             
+for(let i=1;i<=5;i++){
+    for(let j=4;j>=i;j--)
+    {
+        process.stdout.write(String("*"))
+    }
+    console.log("")
+}
+
+/*output
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
+
+for(let i=1; i<=5;i++){
+    for(let j=5;j>=i;j--)
+    {
+    process.stdout.write(String("*"))
+    }
+    console.log("")
+}
+for(let i=2;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(String("*"))
+    }
+    console.log("")
+}
+/* output
+
+*****
+****
+***
+**
+*
+**
+***
+****
+*****
+
+*/
+
+for(let i=1;i<=5;i++){
+    for(let j=5;j>=i;j--)
+    {
+        process.stdout.write(String(" "))
+    }
+    for(let k=1;k<=i;k++){
+        process.stdout.write(String("*"))
+    }
+    console.log(" ")
+    
+}
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(String(" "))
+    }
+    for(let k=4;k>=i;k--){
+        process.stdout.write(String("*"))
+    }
+    console.log("*")
+}
+
+/*
+     * 
+    **
+   ***
+  ****
+ *****
+ *****
+  ****
+   ***
+    **
+     *
+
+    */
+
+
+
+for(let i=1;i<=5;i++){
+    for(let j=5;j>=i;j--){
+        process.stdout.write(String(" "))
+    }
+    for(let k=1;k<=i;k++){
+        process.stdout.write(String("*"))
+    }
+    for(let l=2;l<=i;l++){
+        process.stdout.write(String("*"))
+    }
+    console.log("")
+}
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+
+*/
+
+
+for(let i=1;i<=5;i++){
+        for(let j=1;j<=i;j++){
+            process.stdout.write(String(" "))
+        }
+        for(let k=5;k>=i;k--){
+            process.stdout.write(String("*"))
+        }
+        for(let l=4;l>=i;l--){
+            process.stdout.write(String("*"))
+        }
+        console.log(" ")
+    }
+/*
+
+ ********* 
+  *******
+   *****
+    ***
+     *
+
+*/
+
+
+
+for(let i=1;i<=5;i++){
+    for(let j=5;j>=i;j--){
+        process.stdout.write(String(" "))
+    }
+    for(let k=1;k<=i;k++){
+        process.stdout.write(String("*"))
+    }
+    for(let l=2;l<=i;l++){
+        process.stdout.write(String("*"))
+    }
+    console.log("")
+}
+for(let i=2;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(String(" "))
+    }
+    for(let k=5;k>=i;k--){
+        process.stdout.write(String("*"))
+    }
+    for(let l=4;l>=i;l--){
+        process.stdout.write(String("*"))
+    }
+    console.log(" ")
+}
+
+/*
+
+    *
+    ***
+   *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+
+*/
+
+
+
