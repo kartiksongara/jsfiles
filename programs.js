3a561f109bddcd4e9a1e2dfcc4f90a506901aa79
kartiksongara <kartiksongara2000@gmail.com>
2023-11-05 22:22:56 +0530

    add

---
+++ programs.js
@@ -0,0 +1,67 @@
+// 1 define fibonacci series usinng for loop
+// let a=10;
+// let num=0;
+// for(let i=0;i<=a;i++){
+//     num=num+i
+// }
+// console.log(num)
+
+
+// 2 define prime number using for loop
+
+// let num=49;
+// let flag=0;
+// if(num>1){
+//     for(let i=2;i<=num-1;i++){
+//         if(num%i==0){
+//             flag=1
+//         }
+//     }
+//     if(flag===1){
+//         console.log("not prime")
+//     }
+//     else{
+//         console.log("prime")
+//     }
+// }else{
+//     console.log("not prime")
+// }
+
+
+// 3 define a program print revers number  using while loop 
+
+// let num=12345;
+// let c=0;
+// while (num!=0){
+//     c=c*10 + num%10
+//     num=Math.trunc(num/10)
+    
+// }
+// console.log(c)
+
+
+
+
+// 4 define the total of the given number using while loop
+
+// let num=12345;
+// let c=0
+// while (num!=0) {
+//     c=c+ num%10;
+//     num=Math.trunc(num/10)
+    
+// }
+// console.log(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
