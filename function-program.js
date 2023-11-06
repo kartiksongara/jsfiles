1009716499e3aa0762584457eabb278cbdb50606
kartiksongara <kartiksongara2000@gmail.com>
2023-11-06 18:17:57 +0530

    aaa

---
+++ function-program.js
@@ -0,0 +1,73 @@
+// define function program of even odd 
+
+// function num(a) {
+//     if(a%2===0){
+//         return "even"
+//     }else{
+//         return "odd"
+//     }
+    
+// }
+// console.log(num(10))
+
+
+// define function program of factroial of given number 
+
+// function fact(num){
+//     let fact=1
+//     for(let i=1;i<=num;i++){
+         
+//           fact=i*fact;
+
+//     }
+    
+//     return fact;
+
+
+
+// }
+// console.log(fact(10))
+
+
+// define function program of fibonacci series number of 10
+
+
+// function num(a){
+//     let n=0
+// for(let i=0;i<=a;i++){
+//     n=n+i
+
+// }
+// return n
+// }
+// console.log(num(10))
+
+
+// define function program of prime number 
+
+// let flage
+// function prime(num){
+//     if(num>=1){
+//         for(let i=2;i<=num-1;i++){
+//             if(num%i==0){
+//                flage=0
+//             }
+//     }
+//     if(flage==0){
+//         return "not prime"
+//     }
+//     else{
+//         return "prime"
+//     }
+
+//     }else{
+//         return "not prime"
+//     }
+    
+// }
+// console.log(prime(19))
+
+
+
+
+
